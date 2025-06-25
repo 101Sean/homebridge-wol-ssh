860bffef3bdad0bdbca56594b8a303dbaf9cdb68
mikeryu98 <mikeryu98@gmail.com>
2025-06-25 21:31:57 +0900

    v1.0.1 동적방식

--- index.js
+++ index.js
@@ -1,144 +1,160 @@
+// index.js
 const axios   = require('axios')
 const cheerio = require('cheerio')
 const { Client } = require('ssh2')
 
-let Service, Characteristic
+let Service, Characteristic, UUID
 
 module.exports = (api) => {
-    Service = api.hap.Service
+    Service        = api.hap.Service
     Characteristic = api.hap.Characteristic
-    api.registerAccessory('MyTvPowerSwitch', MyTvPowerSwitch)
+    UUID           = api.hap.uuid
+
+    api.registerPlatform(
+        'homebridge-wol-ssh',   // package.json.name
+        'WolSshPlatform',       // platform identifier
+        WolSshPlatform,
+        true                    // dynamic external accessories
+    )
 }
 
-class MyTvPowerSwitch {
-    constructor(log, config) {
-        this.log    = log
-        this.config = config
+class WolSshPlatform {
+    constructor(log, config, api) {
+        this.log       = log
+        this.config    = config
+        this.api       = api
+        this.accessories = []
 
-        const { domain } = config
-        this.loginUrl   = `${domain}/sess-bin/login_handler.cgi`
-        this.sessionUrl = `${domain}/sess-bin/login_session.cgi?noauto=1`
-        this.listUrl    = `${domain}/sess-bin/timepro.cgi?tmenu=iframe&smenu=expertconfwollist`
-        this.wakeUrl    = `${domain}/sess-bin/timepro.cgi`
-
-        this.informationService = new Service.AccessoryInformation()
-            .setCharacteristic(Characteristic.Manufacturer, 'Custom')
-            .setCharacteristic(Characteristic.Model,        'TV–WOL+SSH')
-
-        this.switchService = new Service.Switch(config.name || 'TV Power')
-        this.switchService.getCharacteristic(Characteristic.On)
-            .on('get', this.handleGet.bind(this))
-            .on('set', this.handleSet.bind(this))
+        api.on('didFinishLaunching', () => this.publishSwitch())
     }
 
-    getServices() {
-        return [ this.informationService, this.switchService ]
+    configureAccessory(accessory) {
+        this.accessories.push(accessory)
     }
 
-    handleGet(cb) {
-        cb(null, false)
+    publishSwitch() {
+        const uuid = UUID.generate(this.config.domain)
+        const acc  = new this.api.platformAccessory(this.config.name, uuid)
+        acc.category = this.api.hap.Categories.SWITCH
+
+        const sw = acc.addService(Service.Switch, this.config.name)
+        sw.getCharacteristic(Characteristic.On)
+            .onGet(() => false)
+            .onSet((value, cb) => this.handlePower(value, cb))
+
+        this.api.publishExternalAccessories('homebridge-wol-ssh', [ acc ])
+        this.log.info('✅ Published WOL-SSH Switch:', this.config.name)
     }
 
-    async handleSet(on, cb) {
-        if (!on) {
-            // OFF: SSH로 원격 종료
+    async handlePower(on, callback) {
+        if (on) {
+            try {
+                await this.doWake()
+                this.log.info('WOL 실행 성공')
+                callback()
+            } catch (err) {
+                this.log.error('WOL 오류', err.message)
+                callback(err)
+            }
+        } else {
             const conn = new Client()
             conn.on('ready', () => {
-                this.log('SSH 연결됨, 종료 명령 전송')
+                this.log.info('SSH 연결됨, 종료 명령 전송')
                 conn.exec('shutdown /s /t 0', (err, stream) => {
                     if (err) {
-                        this.log.error(err)
+                        this.log.error('SSH 오류', err)
                         conn.end()
-                        return cb(err)
+                        return callback(err)
                     }
                     stream.on('close', () => {
-                        this.log('종료 명령 완료')
+                        this.log.info('SSH 종료 성공')
                         conn.end()
-                        cb(null)
+                        callback()
                     })
                 })
             })
                 .on('error', err => {
                     this.log.error('SSH 연결 실패', err)
-                    cb(err)
+                    callback(err)
                 })
                 .connect({
                     host:     this.config.domain.replace(/^https?:\/\//, ''),
                     port:     300,
                     username: 'sean',
-                    // privateKey 옵션 필요 시 uncomment
                     // privateKey: require('fs').readFileSync('/home/homebridge/.ssh/id_rsa')
                 })
-            return
         }
+    }
 
-        try {
-            // ON: 로그인 → 쿠키 → MAC 파싱 → WOL
-            const loginBody = new URLSearchParams({
-                username:       this.config.username,
-                passwd:         this.config.password,
-                init_status:    1,
-                captcha_on:     1,
-                default_passwd: 'admin',
-                Referer:        this.sessionUrl
-            }).toString()
+    // 로그인 → 쿠키 파싱 → MAC 주소 파싱 → WOL 요청
+    async doWake() {
+        const { domain, username, password, targetName } = this.config
 
-            const loginResp = await axios.post(this.loginUrl, loginBody, {
-                headers: {
-                    'Connection':   'keep-alive',
-                    'Content-Type': 'application/x-www-form-urlencoded'
-                }
-            })
+        // 1) 로그인 핸들러 호출
+        const loginUrl  = `${domain}/sess-bin/login_handler.cgi`
+        const sessionUrl= `${domain}/sess-bin/login_session.cgi?noauto=1`
+        const loginBody = new URLSearchParams({
+            username,
+            passwd:         password,
+            init_status:    1,
+            captcha_on:     1,
+            default_passwd: 'admin',
+            Referer:        sessionUrl
+        }).toString()
 
-            const cookieRe = /document\.cookie\s*=\s*'([^']+)'/g
-            const cookies = []
-            let m
-            while (m = cookieRe.exec(loginResp.data)) cookies.push(m[1])
-            if (!cookies.length) throw new Error('세션 쿠키 획득 실패')
-            this.sessionCookies = cookies.join('; ')
-            this.log('세션 쿠키:', this.sessionCookies)
+        const loginResp = await axios.post(loginUrl, loginBody, {
+            headers: {
+                'Connection':   'keep-alive',
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        })
 
-            const listResp = await axios.get(this.listUrl, {
-                headers: {
-                    'Connection':                'keep-alive',
-                    'Upgrade-Insecure-Requests': '1',
-                    'Host':                      new URL(this.listUrl).host,
-                    'Cookie':                    this.sessionCookies
-                }
-            })
+        // 2) document.cookie 스크립트에서 세션 쿠키 추출
+        const cookieRe = /document\.cookie\s*=\s*'([^']+)'/g
+        const cookies = []
+        let m
+        while (m = cookieRe.exec(loginResp.data)) cookies.push(m[1])
+        if (!cookies.length) throw new Error('세션 쿠키 획득 실패')
+        const sessionCookies = cookies.join('; ')
+        this.log.debug('세션 쿠키:', sessionCookies)
 
-            const $ = cheerio.load(listResp.data)
-            $('tr.wol_main_tr').each((_, tr) => {
-                const [mac, name] = $(tr).find('span.wol_main_span')
-                    .toArray().map(el => $(el).text().trim())
-                if (name === this.config.targetName) {
-                    this.macAddress = mac
-                    this.log('MAC 파싱:', mac)
-                }
-            })
-            if (!this.macAddress) throw new Error('MAC 주소 파싱 실패')
+        // 3) 목록 페이지에서 Desktop MAC 파싱
+        const listUrl = `${domain}/sess-bin/timepro.cgi?tmenu=iframe&smenu=expertconfwollist`
+        const listResp = await axios.get(listUrl, {
+            headers: {
+                'Connection':                'keep-alive',
+                'Upgrade-Insecure-Requests': '1',
+                'Host':                      new URL(listUrl).host,
+                'Cookie':                    sessionCookies
+            }
+        })
+        const $ = cheerio.load(listResp.data)
+        let macAddress = null
+        $('tr.wol_main_tr').each((_, tr) => {
+            const [mac, name] = $(tr).find('span.wol_main_span')
+                .toArray().map(el => $(el).text().trim())
+            if (name === targetName) macAddress = mac
+        })
+        if (!macAddress) throw new Error('MAC 주소 파싱 실패')
+        this.log.debug('파싱된 MAC:', macAddress)
 
-            const wakeBody = new URLSearchParams({
-                tmenu:     'iframe',
-                smenu:     'expertconfwollist',
-                nomore:    '0',
-                wakeupchk: this.macAddress,
-                act:       'wake'
-            }).toString()
+        // 4) Wake POST 요청
+        const wakeUrl = `${domain}/sess-bin/timepro.cgi`
+        const wakeBody = new URLSearchParams({
+            tmenu:     'iframe',
+            smenu:     'expertconfwollist',
+            nomore:    '0',
+            wakeupchk: macAddress,
+            act:       'wake'
+        }).toString()
 
-            await axios.post(this.wakeUrl, wakeBody, {
-                headers: {
-                    'Host':         new URL(this.wakeUrl).host,
-                    'Connection':   'keep-alive',
-                    'Cookie':       this.sessionCookies,
-                    'Content-Type': 'application/x-www-form-urlencoded'
-                }
-            })
-            this.log('WOL 실행 성공')
-            cb(null)
-        } catch (e) {
-            this.log.error('ON 오류:', e.message)
-            cb(e)
-        }
+        await axios.post(wakeUrl, wakeBody, {
+            headers: {
+                'Host':         new URL(wakeUrl).host,
+                'Connection':   'keep-alive',
+                'Cookie':       sessionCookies,
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        })
     }
 }
