7daa2292d67b117420e0739d7106ef5196aa0126
mikeryu98 <mikeryu98@gmail.com>
2025-06-26 05:36:24 +0900

    v1.0.4 axios 제거 http요청 native

--- index.js
+++ index.js
@@ -1,25 +1,9 @@
 // index.js
-const http  = require('http')
-const https = require('https')
-const axios = require('axios')
+const http    = require('http')
+const https   = require('https')
 const cheerio = require('cheerio')
+const { URL } = require('url')
 const { Client } = require('ssh2')
-
-// axios 인스턴스: 불완전 HTTP 헤더도 허용
-const axiosInstance = axios.create({
-    httpAgent:  new http.Agent({ allowInsecureHTTPParser: true }),
-    httpsAgent: new https.Agent({ allowInsecureHTTPParser: true }),
-})
-
-// 디버그용 전역 인터셉터 (로그가 너무 많으면 주석 처리)
-// axiosInstance.interceptors.request.use(cfg => {
-//   console.log('▶▶▶ REQUEST ▶▶▶')
-//   console.log(cfg.method.toUpperCase(), cfg.url)
-//   console.log('Headers:', cfg.headers)
-//   console.log('Body:', cfg.data)
-//   console.log('---------------------')
-//   return cfg
-// })
 
 let Service, Characteristic, UUID
 
@@ -52,7 +36,7 @@ class WolSshPlatform {
         const sw = acc.addService(Service.Switch, this.config.name)
         sw.getCharacteristic(Characteristic.On)
             .onGet(() => false)
-            .onSet(value => this.handlePower(value))
+            .onSet(val => this.handlePower(val))
 
         this.api.publishExternalAccessories('homebridge-wol-ssh', [acc])
         this.log.info('✅ Published WOL-SSH Switch:', this.config.name)
@@ -63,7 +47,7 @@ class WolSshPlatform {
             this.log.info('[handlePower] WOL 시작')
             try {
                 await this.doWake()
-                this.log.info('[handlePower] WOL 실행 성공')
+                this.log.info('[handlePower] WOL 완료')
             } catch (e) {
                 this.log.error('[handlePower] WOL 오류:', e.message)
                 throw e
@@ -98,75 +82,83 @@ class WolSshPlatform {
         }
     }
 
+    // raw http/https 요청 유틸
+    httpRequest(options, body = '') {
+        return new Promise((resolve, reject) => {
+            const isHttps = options.protocol === 'https:'
+            const lib     = isHttps ? https : http
+            options.agent = new lib.Agent({ allowInsecureHTTPParser: true })
+            const req = lib.request(options, res => {
+                let data = ''
+                res.on('data', chunk => data += chunk)
+                res.on('end', () => {
+                    this.log.debug(`[httpRequest] ${options.method} ${options.path} -> ${res.statusCode}`)
+                    resolve({ statusCode: res.statusCode, headers: res.headers, body: data })
+                })
+            })
+            req.on('error', reject)
+            if (body) req.write(body)
+            req.end()
+        })
+    }
+
     async doWake() {
         const { domain, wolPort, username, password, targetName } = this.config
-        const url    = new URL(domain)
-        url.port     = wolPort
-        const origin = url.origin           // "http://host:port"
-        const host   = url.host             // "host:port"
+        const url = new URL(domain)
+        url.port = wolPort
+        const origin = url.origin    // "http://host:port"
+        const host   = url.host      // "host:port"
 
-        this.log.info('[doWake] URL 세팅:', origin)
+        this.log.info('[doWake] URL:', origin)
 
-        // 폼 바디 및 헤더 생성 유틸
-        const makeForm = data => {
-            const body = new URLSearchParams(data).toString()
-            return {
-                body,
-                headers: {
-                    'Accept':            'text/html',
-                    Host:                host,
-                    Connection:          'close',
-                    'Content-Type':      'application/x-www-form-urlencoded',
-                    'Content-Length':    Buffer.byteLength(body).toString()
-                }
-            }
-        }
-
-        // 1) 로그인
-        this.log.info('[doWake] 1) 로그인 요청 준비')
-        const loginData = {
+        // --- 1) 로그인 ---
+        const loginPath = `${url.pathname}/sess-bin/login_handler.cgi`
+        const loginData = new URLSearchParams({
             username, passwd: password,
             init_status:1, captcha_on:1, default_passwd:'admin',
             Referer: `${origin}/sess-bin/login_session.cgi?noauto=1`
-        }
-        const { body: loginBody, headers: loginHeaders } = makeForm(loginData)
-        this.log.debug('[doWake] 로그인 헤더:', loginHeaders)
-        this.log.debug('[doWake] 로그인 바디:', loginBody)
+        }).toString()
+        this.log.info('[doWake] 1) 로그인 요청')
+        const loginResp = await this.httpRequest({
+            protocol:  url.protocol,
+            hostname:  url.hostname,
+            port:      url.port,
+            method:    'POST',
+            path:      loginPath,
+            headers: {
+                'Accept':            'text/html',
+                'Host':              host,
+                'Connection':        'close',
+                'Content-Type':      'application/x-www-form-urlencoded',
+                'Content-Length':    Buffer.byteLength(loginData)
+            }
+        }, loginData)
 
-        const loginResp = await axiosInstance.post(
-            `${origin}/sess-bin/login_handler.cgi`,
-            loginBody,
-            { headers: loginHeaders }
-        )
-        this.log.info('[doWake] 1) 로그인 응답 수신, HTML 길이:', loginResp.data.length)
-
-        // 세션 ID 추출
-        const match = loginResp.data.match(/setCookie\('([^']+)'\)/)
-        if (!match) {
-            this.log.error('[doWake] 세션 쿠키 패턴 매칭 실패')
-            throw new Error('세션 쿠키 획득 실패')
-        }
+        this.log.debug('[doWake] 로그인 응답 헤더:', loginResp.headers)
+        const match = loginResp.body.match(/setCookie\('([^']+)'\)/)
+        if (!match) throw new Error('세션 쿠키 획득 실패')
         const sessionId = match[1].trim()
         this.log.info('[doWake] 세션 ID:', sessionId)
 
-        // 2) MAC 목록 조회
-        this.log.info('[doWake] 2) MAC 목록 GET 요청 준비')
-        const getHeaders = {
-            'Accept':   'text/html',
-            Host:       host,
-            Connection: 'close',
-            Cookie:     `efm_session_id=${sessionId}`
-        }
-        this.log.debug('[doWake] GET 헤더:', getHeaders)
+        // --- 2) MAC 목록 조회 ---
+        const listPath = `${url.pathname}/sess-bin/timepro.cgi?tmenu=iframe&smenu=expertconfwollist`
+        this.log.info('[doWake] 2) 목록 요청')
+        const listResp = await this.httpRequest({
+            protocol: url.protocol,
+            hostname: url.hostname,
+            port:     url.port,
+            method:   'GET',
+            path:     listPath,
+            headers: {
+                'Accept':     'text/html',
+                'Host':       host,
+                'Connection': 'close',
+                'Cookie':     `efm_session_id=${sessionId}`
+            }
+        })
+        this.log.debug('[doWake] 목록 응답 길이:', listResp.body.length)
 
-        const listResp = await axiosInstance.get(
-            `${origin}/sess-bin/timepro.cgi?tmenu=iframe&smenu=expertconfwollist`,
-            { headers: getHeaders }
-        )
-        this.log.info('[doWake] 2) 목록 응답 수신, HTML 길이:', listResp.data.length)
-
-        // 파싱
-        const $ = cheerio.load(listResp.data)
+        const $ = cheerio.load(listResp.body)
         let mac = null
         $('tr.wol_main_tr').each((_, tr) => {
             const desc = $(tr).find('td').eq(2).find('.wol_main_span').text().trim()
@@ -175,31 +167,31 @@ class WolSshPlatform {
                 return false
             }
         })
-        if (!mac) {
-            this.log.error('[doWake] MAC 파싱 실패, targetName:', targetName)
-            throw new Error(`"${targetName}"에 해당하는 MAC 주소 파싱 실패`)
-        }
-        this.log.info('[doWake] 대상 MAC 주소:', mac)
+        if (!mac) throw new Error(`MAC 파싱 실패: ${targetName}`)
+        this.log.info('[doWake] 대상 MAC:', mac)
 
-        // 3) WOL POST
-        this.log.info('[doWake] 3) WOL POST 요청 준비')
-        const wakeData = {
-            tmenu:'iframe',
-            smenu:'expertconfwollist',
-            nomore:0,
-            wakeupchk:mac,
-            act:'wake'
-        }
-        const { body: wakeBody, headers: wakeHeaders } = makeForm(wakeData)
-        wakeHeaders.Cookie = `efm_session_id=${sessionId}`
-        this.log.debug('[doWake] WOL 헤더:', wakeHeaders)
-        this.log.debug('[doWake] WOL 바디:', wakeBody)
-
-        const wakeResp = await axiosInstance.post(
-            `${origin}/sess-bin/timepro.cgi`,
-            wakeBody,
-            { headers: wakeHeaders }
-        )
-        this.log.info('[doWake] 3) WOL 응답 상태코드:', wakeResp.status)
+        // --- 3) WOL POST ---
+        const wakePath = `${url.pathname}/sess-bin/timepro.cgi`
+        const wakeData = new URLSearchParams({
+            tmenu:'iframe', smenu:'expertconfwollist',
+            nomore:0, wakeupchk:mac, act:'wake'
+        }).toString()
+        this.log.info('[doWake] 3) WOL 요청')
+        const wakeResp = await this.httpRequest({
+            protocol: url.protocol,
+            hostname: url.hostname,
+            port:     url.port,
+            method:   'POST',
+            path:     wakePath,
+            headers: {
+                'Accept':         'text/html',
+                'Host':           host,
+                'Connection':     'close',
+                'Content-Type':   'application/x-www-form-urlencoded',
+                'Content-Length': Buffer.byteLength(wakeData),
+                'Cookie':         `efm_session_id=${sessionId}`
+            }
+        }, wakeData)
+        this.log.info('[doWake] WOL 응답 코드:', wakeResp.statusCode)
     }
 }
